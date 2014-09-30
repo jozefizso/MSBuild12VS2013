69723cb7001abde53d81110cba83069f5a7d7e81
Jozef Izso <jozef.izso@gmail.com>
2014-09-30 12:18:55 +0200

    Implemented sample task.

---
+++ MSBuild12VS2013/SampleTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild12VS2013
+{
+    public class SampleTask : Task
+    {
+        public override bool Execute()
+        {
+            this.Log.LogMessage("Message from sample task.");
+
+            return true;
+        }
+    }
+}
